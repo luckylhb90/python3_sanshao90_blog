b45a1840558bd3349d70819fd53b17f7617c706d
lucky <luckylhb90@gmail.com>
2017-12-24 14:09:18 +0800

    Step6: blog index view --修改返回html页面

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("欢迎访问我的博客首页！")
+    # return HttpResponse("欢迎访问我的博客首页！")
+    body = {'title': 'SanShao Blog', 'welcome': '欢迎访问我的博客首页'}
+    return render(request, 'blog/index.html', context=body)

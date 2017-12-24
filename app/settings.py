f34f6507f7132651aa887dad2c13de5e9a61ee7c
lucky <luckylhb90@gmail.com>
2017-12-24 12:11:03 +0800

    Step5: make mysql models --增加mysql配置 --pip install mysqlclient

--- app/settings.py
+++ app/settings.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
 Django settings for app project.
 
@@ -71,11 +73,23 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',# 或者使用 mysql.connector.django
+        'NAME': 'sanshao90_blog',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'PORT': '3306',
+
     }
 }
 
